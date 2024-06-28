59ebdd0452b78d97bf5f6aed57e94768d9c1eee9
Anshcodee <131477514+Anshcodee@users.noreply.github.com>
2024-06-28 08:53:51 -0400

    message

---
+++ 1-SampleFolder/sample_folder.js
@@ -0,0 +1,12 @@
+/*In this lab, you will develop an understanding of JavaScript variables, including var, let, and const. 
+JavaScript variables act as containers for storing data, allowing dynamic programming by facilitating the storage, manipulation, and retrieval of values across various sections of code within applications. 
+You will learn how to use variables to enhance code readability and maintainability, enabling efficient management and reuse of variable values throughout your entire programs.
+*/
+
+/* Uncompleted assignment:
+Create one block using {} and declare variables using let, const, and var.
+
+Then, try to reassign these variables within the curly braces {}. Check for any errors that may occur.
+
+Next, try to reassign the same variables outside the block {} and check if assignment outside the sope of variables where the variables are assigned can be done or not.
+*/
