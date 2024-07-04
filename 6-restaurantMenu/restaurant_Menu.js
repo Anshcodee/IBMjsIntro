ebf1734c22b0455f774af74f826583df03a34ebb
Anshcodee <131477514+Anshcodee@users.noreply.github.com>
2024-07-04 16:48:45 -0400

    message

---
+++ 6-restaurantMenu/restaurant_Menu.js
@@ -0,0 +1,7 @@
+//In this lab, you will explore how JavaScript dynamically generates restaurant menu sections for breakfast, main course, and dessert using JavaScript's array methods. 
+//You will learn to use map to populate the breakfast menu, forEach to display the main course menu items, and a traditional for loop to render the dessert items. 
+//Additionally, you will understand how HTML content can be updated dynamically, showcasing the power of JavaScript in modifying webpage elements based on predefined arrays.
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak','Pasta','Burger','Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
