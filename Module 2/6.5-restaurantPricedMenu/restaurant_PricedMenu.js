5adf4fe6afa062096fc8aa4cb3090bd94c6b8f01
Anshcodee <131477514+Anshcodee@users.noreply.github.com>
2024-07-05 04:54:25 -0400

    message

---
+++ Module 2/6.5-restaurantPricedMenu/restaurant_PricedMenu.js
@@ -0,0 +1,31 @@
+const breakfastMenu = ['Pancakes -$12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak -$30', 'Pasta -$24.99', 'Burger -$25', 'Salmon -$28'];
+const dessertMenu = ['Cake -$30', 'Ice Cream -$8', 'Pudding -$12', 'Fruit Salad -$15'];
+
+
+// Using 'map' method for breakfastMenu
+
+const breakfastMenuItems_HTML = breakfastMenu.map((item,index) => `<p>--> Item ${index+1}: ${item}`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItems_HTML;
+// join('') concatenates all strings into one
+// .innerHTML --> 
+
+
+// Using 'forEach' method for mainCourseMenu
+
+let mainCourseMenuItems_HTML = '';
+mainCourseMenu.forEach((item,index) => {
+    mainCourseMenuItems_HTML += `<p>--> Item ${index+1}: ${item}`;
+});
+document.getElementById('mainCourseMenuItems').innerHTML = mainCourseMenuItems_HTML;
+
+// forEach adds to an already existing variable. One by one
+
+
+// Using a cute for loop for desserts
+
+let dessertMenuItems_HTML = '';
+for (i=0; i<dessertMenu.length;i++){
+    dessertMenuItems_HTML += `<p>--> Item ${i+1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertMenuItems_HTML;
+
