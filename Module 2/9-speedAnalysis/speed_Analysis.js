40f7d94dcbdabbd77ed40f700e0ad75df45b7e0e
Anshcodee <131477514+Anshcodee@users.noreply.github.com>
2024-07-06 04:46:11 -0400

    message

--- Module 2/9-speedAnalysis/speed_Analysis.js
+++ Module 2/9-speedAnalysis/speed_Analysis.js
@@ -20,5 +20,40 @@ function startTest(){
     var button = document.getElementById("btn");
     button.innerHTML = "End Test"
     button.onclick = endTest;
-    
+}
+
+function endTest(){
+    endTime = new Date().getTime();
+
+    //Disable further input
+    document.getElementById("userInput").readOnly= true;
+
+    // Calculate time elapsed and Words per minute(WPM)
+    var timeElapsed = (endTime - startTime)/1000 //in seconds
+    var userTypedText = document.getElementById("userInput").value;
+
+    //Split the text using regex to count words correctly
+    var typedwords = userTypedText.split(/\s+/).filter(function(word){
+        return word !=="";
+    }).length
+    // \s+ identifies all whitespaces(space, tabs,etc...)
+
+    var wpm = 0; //A default value
+
+    if(timeElapsed!==0 && !isNaN(typedwords)){
+        wpm = Math.round((typedWords/timeElapsed)*60);
+    }
+
+    // Display result
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = "<h2>Typing Test results: </h2><hr><br>"+
+    "<p> Words Typed: "+ typedwords + "</p>"+
+    "<p>Time Elapsed: "+timeElapsed.toFixed(2)+"seconds</p>"
+    //toFixed(x) gives a decimal value having x decimal digits
+    "<p>Words per Minute(WPM): "+ wpm +"</p>";
+
+    //Resetting the button
+    var button = document.getElementById("btn");
+    button,innerHTML = "Start Test";
+    button.onclick = startTest;
 }
